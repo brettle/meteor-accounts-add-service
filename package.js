9b8fa4c94fa9ca1e430625d82101391e6624014a
Dean Brettle <dean@brettle.com>
2015-08-14 15:15:21 -0700

    Claim support for Meteor 1.0.4 or later.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'brettle:accounts-add-service',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'Allow users to add login services to their accounts',
   git: 'https://github.com/brettle/meteor-accounts-add-service.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.0.4');
   api.use('accounts-base');
   api.use('brettle:accounts-multiple@0.0.1', 'server');
   api.export('AccountsAddService');
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.0.4');
   api.use('tinytest');
   api.use('brettle:accounts-add-service@0.1.0');
   api.use('brettle:accounts-testing-support@0.1.0');
