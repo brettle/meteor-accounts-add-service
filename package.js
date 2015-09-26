060a2aacc455a055af55338d3230a6229930d99d
Dean Brettle <dean@brettle.com>
2015-09-26 14:46:05 -0700

    Publish 0.3.3 which references versions that work with Meteor 1.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'brettle:accounts-add-service',
-  version: '0.3.2',
+  version: '0.3.3',
   summary: 'Allow users to add login services to their accounts',
   git: 'https://github.com/brettle/meteor-accounts-add-service.git',
   documentation: 'README.md'
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   api.use('brettle:workaround-issue-4331@0.0.1');
   api.use('brettle:workaround-issue-4970@0.0.1');
   api.use('brettle:workaround-issue-5110@0.0.1');
-  api.use('brettle:accounts-multiple@0.0.1', 'server');
+  api.use('brettle:accounts-multiple@0.3.1', 'server');
   api.export('AccountsAddService');
   api.addFiles('accounts-add-service-common.js');
   api.addFiles('accounts-add-service-client.js', 'client');
@@ -25,8 +25,8 @@ Package.onTest(function(api) {
   api.versionsFrom('1.0.4');
   api.use('tinytest');
   api.use('ddp');
-  api.use('brettle:accounts-add-service@0.2.0');
-  api.use('brettle:accounts-testing-support@0.1.0');
-  api.use('brettle:accounts-multiple@0.1.0', 'server');
+  api.use('brettle:accounts-add-service@0.3.3');
+  api.use('brettle:accounts-testing-support@0.4.1');
+  api.use('brettle:accounts-multiple@0.3.1', 'server');
   api.addFiles('accounts-add-service-server-tests.js', 'server');
 });
